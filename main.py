46898ef4ea93d4057372f7fe519d52b455fc0dc3
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2020-03-28 14:58:34 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-num1 = int(input("Digite el numero a:"))
-num2 = int(input("Digite el numero c:"))
+a = int(input("Digite el numero a:"))
+b = int(input("Digite el numero b:"))
+c = int(input("Digite el numero c:"))
 
-producto = num1 * num2
-doble = num1 * 2
+import math
+d = int(input("Digite el numero d:"))
+
+producto = a * c
+doble = a * 2
+cuadrado = b**2
+raiz = math.sqrt(d)
 
 print("El producto de los numeros a y c es:",producto)
 print("El doble del nuemero a es:",doble)
+print("El cuadrado del numero b es:",cuadrado)
+print("La raiz del numero d es:",raiz)
+
+
