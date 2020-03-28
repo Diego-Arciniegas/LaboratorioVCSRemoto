d8fd234238b1ffeeb5b2c7cd0dc10451c781ee31
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2020-03-28 14:09:53 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite el numero a:"))
+num2 = int(input("Digite el numero c:"))
+
+producto = num1 * num2
+doble = num1 * 2
+
+print("El producto de los numeros a y c es:",producto)
+print("El doble del nuemero a es:",doble)
