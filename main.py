c329694015e9e57f110c7f1690624986b246f9d4
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2020-03-28 15:55:50 -0500

    solucion

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-a = int(input("Digite el numero a:"))
-b = int(input("Digite el numero b:"))
-c = int(input("Digite el numero c:"))
+from math import sqrt
 
-import math
-d = int(input("Digite el numero d:"))
+a = int(input("ingrase el primer valor:"))
+b = int(input("ingrase el primer valor:"))
+c = int(input("ingrase el primer valor:"))
+d=((b**2)-4*a*c)
 
-producto = a * c
-doble = a * 2
-cuadrado = b**2
-raiz = math.sqrt(d)
-
-print("El producto de los numeros a y c es:",producto)
-print("El doble del nuemero a es:",doble)
-print("El cuadrado del numero b es:",cuadrado)
-print("La raiz del numero d es:",raiz)
-
+if ((b**2)-4*a*c) > 0:
+    x1 = (-b+sqrt(b**2-(4*a*c)))/(2*a)
+    x2 = (-b-sqrt(b**2-(4*a*c)))/(2*a)
+    print("Los valores de x son:")
+    print("valor de x1:",x1)
+    print("valor de x2:",x2)
+elif ((b**2)-4*a*c) == 0:
+    z = (-b/(2*a))
+    print("x1 y x2 son iguales y corresponden:",z)
+elif ((b**2)-4*a*c) < 0:
+    print("no se puede resolver la ecuacion")
 
